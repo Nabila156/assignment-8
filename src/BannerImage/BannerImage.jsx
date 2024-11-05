b967afb5a44793a231fd6055f8c2085c1bb29f86
Nabila <nowshinnabila24@gmail.com>
2024-11-05 16:41:41 +0600

    footer done

--- src/BannerImage/BannerImage.jsx
+++ src/BannerImage/BannerImage.jsx
@@ -1,8 +1,8 @@
 
 const BannerImage = () => {
     return (
-        <div className="relative bottom-20 left-28 md:left-48 lg:left-[300px]">
-            <img className="h-80 w-[50%] backdrop-blur p-4 border rounded-xl" src="/images/banner.jpg" alt="gadget" />
+        <div className="relative bottom-20">
+            <img className="h-80 mx-auto w-[50%] backdrop-blur p-4 border rounded-xl" src="/images/banner.jpg" alt="gadget" />
         </div>
     );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,35 @@
 
 const Footer = () => {
     return (
-            <footer className="footer px-44 bg-white text-base-content p-10">
+        <div  className=" px-44 bg-white">
+
+            <div className="text-center">
+                <h4 className="text-black pt-12 mb-4 font-bold text-3xl">Gadget Heaven</h4>
+                <p className="mb-12">Leading the way in cutting-edge technology and innovation.</p>
+            </div>
+
+            <footer className="footer text-base-content">
                 <nav>
                     <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <a className="link link-hover">Product Support</a>
+                    <a className="link link-hover">Order Tracking</a>
+                    <a className="link link-hover">Shipping & Delivery</a>
+                    <a className="link link-hover">Returns</a>
                 </nav>
                 <nav>
                     <h6 className="footer-title">Company</h6>
                     <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Careers</a>
                     <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
                 </nav>
                 <nav>
                     <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Terms of Service</a>
                     <a className="link link-hover">Privacy policy</a>
                     <a className="link link-hover">Cookie policy</a>
                 </nav>
             </footer>
+        </div>
     );
 };
 
